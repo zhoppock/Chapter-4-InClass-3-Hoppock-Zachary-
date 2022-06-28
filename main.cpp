e2f859fe3012fd0f75c2acbaf5e20ee4c269163d
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-28 01:18:09 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Hoppock, Zachary
+// doWhileLoop.cpp
+// 10/8/2020
+// Program will do a do-while loop on how many numbers the variable "i" is
+// Version # 1
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+  int i = 0;
+  do
+  {
+    cout << "i is = " << i << endl;
+    i++;
+  }
+  while (i < 10);
+  return 0;
+}
